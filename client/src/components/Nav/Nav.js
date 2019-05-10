f1ad6859df85723691b42f935532e4cb9b0f768e
Ashley <ashleymwilloughby@gmail.com>
2019-05-09 19:44:09 -0500

    fixed className

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -5,7 +5,7 @@ function Nav(props) {
     const home = window.location.pathname.length === 1;
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-            {home ? <h1 class="navbar-brand">Choose Your Character</h1> :
+            {home ? <h1 className="navbar-brand">Choose Your Character</h1> :
                 <a className="navbar-brand" href="/">Back to Character List</a>
             }
         </nav>
